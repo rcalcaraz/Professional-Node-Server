43ac5e2251f57296cf0f78a1ec6ad923f566f944
Rafael Castillo <rafael.castillo@redegal.com>
2017-02-13 09:44:51 +0100

    Continuo la creacion de usuario

--- server/dao/user.js
+++ server/dao/user.js
@@ -7,5 +7,24 @@ module.exports = {
         User.find({}, function(err, users) {
             callback(err, users);
         });
+    },
+
+    getById: function(id, callback) {
+        User.findById(id, function(err, user) {
+            callback(err, user);
+        })
+    },
+
+    create: function(user, callback) {
+        var newUser = new User(user);
+        newUser.save(function(err, user) {
+            callback(err, user);
+        })
+    },
+
+    delete: function(id, callback) {
+        User.findByIdAndRemove(id, function(err, user) {
+            callback(err, user);
+        });
     }
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,11 @@
 var userRest = require('../rest/user.js');
 
 module.exports = function(app) {
-    app.get('/api/users', userRest.getUsers);
+    app.route("/users")
+        .get(userRest.getAll);
+    app.route("/user")
+        .post(userRest.create);
+    app.route("/user/:id")
+        .get(userRest.getById)
+        .delete(userRest.delete);
 }
