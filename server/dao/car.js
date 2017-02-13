69c02c0b5d0cf91276f60abceb8a98c495266005
Rafael Castillo <rafael.castillo@redegal.com>
2017-02-13 11:56:05 +0100

    Actualizo la version de mongoose por un bug

--- server/dao/car.js
+++ server/dao/car.js
@@ -2,11 +2,10 @@
 var car = require('../model/car.js');
 
 module.exports = {
-	
-	getAll: function(callback){
-		car.find({}, function (err, cars) {
-  			callback(err,cars);
-		}); 
-	}
-}
 
+    getAll: function(callback) {
+        car.find({}, function(err, cars) {
+            callback(err, cars);
+        });
+    }
+}
